a5e6d6bd728348b8f04c7337f44a4683bfc9c26d
ddfigueiras <diogo.figueiras.silva@gmail.com>
2023-12-15 00:20:20 +0000

    Extra files deleted

--- obj/Debug/net8.0/Sync.AssemblyInfo.cs
+++ obj/Debug/net8.0/Sync.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sync")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a497d84896d4569337dbc8b467b8fd5103c0070b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sync")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sync")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
